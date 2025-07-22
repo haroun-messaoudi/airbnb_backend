36c00168395f290ca534dd6606dfd7467402d831
haroun-messaoudi <131620959+haroun-messaoudi@users.noreply.github.com>
2025-07-22 16:36:26 +0200

    Update settings.py

--- pfe/pfe/settings.py
+++ pfe/pfe/settings.py
@@ -137,8 +137,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 import os
 
 # Actual directory user files go to
